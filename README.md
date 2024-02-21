2f1d6d382a22368d9d6ee7f0837d3c1c19d9511d
gveronezg <gabrielveronezgiolo@gmail.com>
2024-02-21 17:41:15 -0300

    criando a home

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TGemDJANGO
 Este é o sistema definitivo para TG a ser apresentado!
+
+para restaurar
+    git reset --hard origin/main
--- mypet/urls.py
+++ mypet/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuarios/', include('usuarios.urls'))
+    path('home/', include('home.urls')),
+    path('usuarios/', include('usuarios.urls')),
+    path('', lambda requests: redirect('/home/home'))
 ]
